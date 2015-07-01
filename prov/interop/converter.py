ea6cf66bf67fdd53b48c3d5cc97f75617c11261d
Mike Jackson <mikezonk@gmail.com>
2015-07-01 15:05:17 +0100

    Added converter.py and test_converter.py

--- prov/interop/converter.py
+++ prov/interop/converter.py
@@ -1,4 +1,4 @@
-"""Base class for converters.
+"""Base class, and related classes, for converters.
 """
 # Copyright (c) 2015 University of Southampton
 #
@@ -25,58 +25,74 @@
 from prov.interop.component import ConfigurableComponent
 
 class Converter(ConfigurableComponent):
-  """Base class for converters.
-  """
+  """Base class for converters."""
 
   def __init__(self):
-    """Subclass-specific initialisation.
-    :param self: Object reference
-    :type self: Converter
+    """Create component.
+    Can be overriden by sub-classes.
     """
     super(ConfigurableComponent, self).__init__()
+    print("Converter.__init__")
 
   def get_input_formats(self):
     """Gets list of input formats supported by the converter.
-    :param self: Object reference
-    :type self: Converter
-    :returns list of ctrings
+
+    :returns: formats
+    :rtype: list of str or unicode
     """
     pass
 
   def get_output_formats(self):
     """Gets list of output formats supported by the converter.
-    :param self: Object reference
-    :type self: Converter
-    :returns list of ctrings
+
+    :returns: formats
+    :rtype: list of str or unicode
     """
     pass
 
   def configure(self, config):
-    """Subclass-specific configuration.
-    Extracts sub-class-specific configuration from config, ignore the
-    rest.
-    :param self: Object reference
-    :type self: ConfigurableComponent
+    """Configure converter.
+    Can be overriden by sub-classes.
+
     :param config: Configuration
-    :type config: dictionary
-    :raises ConfigError: if there is missing or invalid configuration
+    :type config: dict
+    :raises ConfigError: if config is not a dict.
     """
     super(Converter, self).configure(config)
 
   def convert(self, in_file, in_format, out_file, out_format):
-    """Invoke subclass-specific conversion.
+    """Invoke conversion of input file in given format to output
+    file in given format.
 
-    :param self: Object reference
-    :type self: Converter
     :param in_file: Input file name
-    :type in_file: string
+    :type in_file: str or unicode
     :param in_format: Input format
-    :type in_format: string
+    :type in_format: str or unicode
     :param out_file: Output file name
-    :type out_file: string
+    :type out_file: str or unicode
     :param out_format: Output format
-    :type out_format: string
-    :returns: True (success) or False (fail).
+    :type out_format: str or unicode
+    :returns: True (success) or False (fail)
+    :rtype: bool
     :raises ConverterError: if there are problems invoking the converter 
     """
     pass
+
+class ConversionError(Exception):
+  """Conversion error."""
+
+  def __init__(self, value):
+    """Create conversion error.
+
+    :param value: Value holding information about error
+    :type value: str or unicode or list of str or unicode
+    """
+    self._value = value
+
+  def __str__(self):
+    """Get error as formatted string.
+
+    :returns: formatted string
+    :rtype: str or unicode
+    """
+    return repr(self._value)
---
+++ tests/prov/interop/test_converter.py
@@ -0,0 +1,43 @@
+"""Test class for prov.interop.converter.Converter.
+"""
+# Copyright (c) 2015 University of Southampton
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions: 
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software. 
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.  
+
+import unittest
+
+from prov.interop.component import ConfigError
+from prov.interop.converter import ConversionError
+from prov.interop.converter import Converter
+
+class ConverterTestCase(unittest.TestCase):
+
+  def test_configure(self):
+    """Test configure method."""
+    comp = Converter()
+    comp.configure({})
+    self.assertTrue(True)
+
+  def test_configure_throws_error(self):
+    """Test configure method with non-dictionary argument."""
+    comp = Converter()
+    with self.assertRaises(ConfigError):
+      comp.configure(123)
