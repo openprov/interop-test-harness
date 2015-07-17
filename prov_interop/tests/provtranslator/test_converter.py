3e4becbe6c23429ba10f5ea11e001956cd73a715
Mike Jackson <mikezonk@gmail.com>
2015-07-17 16:03:46 +0100

    Added converter tests using requests_mock and nose_parameterized

--- prov_interop/tests/provtranslator/test_converter.py
+++ prov_interop/tests/provtranslator/test_converter.py
@@ -24,13 +24,40 @@
 
 import inspect
 import os
+import requests
+import requests_mock
 import tempfile
 import unittest
+
+from nose.tools import nottest
+
+from nose_parameterized import parameterized
+
 
 from prov_interop import standards
 from prov_interop.component import ConfigError
 from prov_interop.converter import ConversionError
 from prov_interop.provtranslator.converter import ProvTranslatorConverter
+
+@nottest
+def test_case_name(testcase_func, param_num, param):
+  """``nose_parameterized`` callback function to create custom
+  test function names.
+
+  :param testcase__func: test function
+  :type testcase__func: function
+  :param param_num: number of parameters in ``param``
+  :type param_num: int
+  :param param: tuple of arguments to test function of form (FORMAT, _)
+  :type param: tuple, assumed to be of form (str or unicode, _)
+  :returns: test function name of form ``testcase_func_FORMAT``
+  :rtype: str or unicode
+  """
+  (format, _) = param.args
+  return "%s_%s" %(
+    testcase_func.__name__,
+    parameterized.to_safe_name(str(format)))
+
 
 class ProvTranslatorConverterTestCase(unittest.TestCase):
 
@@ -40,7 +67,8 @@ class ProvTranslatorConverterTestCase(unittest.TestCase):
     self.in_file = None
     self.out_file = None
     self.config = {}  
-    self.config[ProvTranslatorConverter.URL] = "http://myurl"
+    self.config[ProvTranslatorConverter.URL] = \
+        "https://" + self.__class__.__name__ + "/converter"
     self.config[ProvTranslatorConverter.INPUT_FORMATS] = list(
       standards.FORMATS)
     self.config[ProvTranslatorConverter.OUTPUT_FORMATS] = list(
@@ -86,3 +114,57 @@ class ProvTranslatorConverterTestCase(unittest.TestCase):
     self.out_file = "convert_invalid_output_format.nosuchformat"
     with self.assertRaises(ConversionError):
       self.provtranslator.convert(self.in_file, self.out_file)
+
+  def test_convert(self):
+    self.provtranslator.configure(self.config)
+    (_, self.in_file) = tempfile.mkstemp(suffix="." + standards.JSON)
+    (_, self.out_file) = tempfile.mkstemp(suffix="." + standards.JSON)
+    doc = "mockDocument"
+    # Set up mock service response.
+    headers={"Content-Type": 
+             ProvTranslatorConverter.CONTENT_TYPES[standards.JSON],
+             "Accept": 
+             ProvTranslatorConverter.CONTENT_TYPES[standards.JSON]}
+    with requests_mock.Mocker(real_http=False) as mocker:
+      mocker.register_uri("POST", 
+                          self.config[ProvTranslatorConverter.URL],
+                          text=doc)
+      self.provtranslator.convert(self.in_file, self.out_file)
+      with open(self.out_file, 'r') as f:
+        self.assertEqual(doc, f.read(), "Unexpected output file content")
+
+  @parameterized.expand([
+      (standards.PROVN, ProvTranslatorConverter.CONTENT_TYPES[standards.PROVN]),
+      (standards.TTL, ProvTranslatorConverter.CONTENT_TYPES[standards.TTL]),
+      (standards.TRIG, ProvTranslatorConverter.CONTENT_TYPES[standards.TRIG]),
+      (standards.PROVX, ProvTranslatorConverter.CONTENT_TYPES[standards.PROVX]),
+      (standards.JSON, ProvTranslatorConverter.CONTENT_TYPES[standards.JSON])
+      ], testcase_func_name=test_case_name)
+  def test_convert(self, format, content_type):
+    self.provtranslator.configure(self.config)
+    (_, self.in_file) = tempfile.mkstemp(suffix="." + format)
+    (_, self.out_file) = tempfile.mkstemp(suffix="." + format)
+    doc = "mockDocument"
+    # Set expected headers for given input and output formats.
+    headers={"Content-Type": content_type,
+             "Accept": content_type}
+    # Set up mock service response.
+    with requests_mock.Mocker(real_http=False) as mocker:
+      mocker.register_uri("POST", 
+                          self.config[ProvTranslatorConverter.URL],
+                          text=doc)
+      self.provtranslator.convert(self.in_file, self.out_file)
+      with open(self.out_file, 'r') as f:
+        self.assertEqual(doc, f.read(), "Unexpected output file content")
+
+  def test_convert_server_error(self):
+    self.provtranslator.configure(self.config)
+    (_, self.in_file) = tempfile.mkstemp(suffix="." + standards.JSON)
+    (_, self.out_file) = tempfile.mkstemp(suffix="." + standards.JSON)
+    # Set up mock service response with server error.
+    with requests_mock.Mocker(real_http=False) as mocker:
+      mocker.register_uri("POST", 
+                          self.config[ProvTranslatorConverter.URL],
+                          status_code=requests.codes.internal_server_error)
+      with self.assertRaises(ConversionError):
+        self.provtranslator.convert(self.in_file, self.out_file)
