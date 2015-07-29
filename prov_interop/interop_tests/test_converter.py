a7798524d7e87b248d6da38a4776cb2d83babfc1
Mike Jackson <mikezonk@gmail.com>
2015-07-29 17:39:56 +0100

    Fixed bug

--- prov_interop/interop_tests/test_converter.py
+++ prov_interop/interop_tests/test_converter.py
@@ -282,4 +282,4 @@ class ConverterTestCase(unittest.TestCase):
     self.converter.convert(file_ext_in, self.converter_ext_out)
     comparator = harness.harness_resources.format_comparators[ext_out]
     are_equivalent = comparator.compare(file_ext_out, self.converter_ext_out)
-    self.assertTrue(are_equivalent, msg=msg)
+    self.assertTrue(are_equivalent)
